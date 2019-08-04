464d940666f50b47356c854ec2f6ed69e7fa5e86
Clint Brodar <clintbrodar@gmail.com>
2019-08-04 12:20:54 -0700

    Footer

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import React, { Component } from "react";
 import Wrapper from './components/Wrapper'
 import Header from './components/Header'
 import Main from './components/Main'
+import Footer from './components/Footer'
 
 class App extends Component {
   render() {
     return (
-
       <div>
-      <Wrapper />
-
-      <Header/>
-      <Main />
-
+        <Wrapper />
+        <Header />
+        <Main />
+        <Footer />
       </div>
     )
   }
