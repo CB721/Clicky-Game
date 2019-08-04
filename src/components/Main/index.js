03e9c2e99132c4bc3ff47d194c30c87c9e411dee
Clint Brodar <clintbrodar@gmail.com>
2019-08-04 13:12:23 -0700

    Characters displayed

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import "./style.css";
+import Card from "../Card";
+import characters from "./characters.json";
 
 // change to class component to handle states
 // setState() this.state.name = Clint, this.setState({name:"Clint"})
-function here(){
-    console.log("Here");
-}
 
-function Main() {
-    return <div onClick={here} className="container">
+class Main extends Component {
+    // here = () => {
+    //     console.log("Here");
+    // }
+    state = {
+        characters
+    };
 
-    
-        {/* Populate with 12 cards - use map function */}
-    </div>
-    
+    render() {
+        return <div className="container">
+            {this.state.characters.map(character => (
+                <Card
+                    id={character.id}
+                    image={character.image}
+                />
+            ))}
+
+            {/* Populate with 12 cards - use map function */}
+        </div>
+    }
+
+
 }
 
 export default Main;
