d59f2d08bed6384402fca97ed46f86aa6468ceb3
Clint Brodar <clintbrodar@gmail.com>
2019-08-04 16:00:27 -0700

    Character id onclick

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import "./style.css";
 
 function Card(props) {
+
+  const characterSelction = (id) => {
+    console.log(id);
+  };
     return (
-        <div className="card">
+        <div className="card popout">
           <div className="img-container">
-            <img alt={props.name} src={props.image} />
+            <img alt={props.name} src={props.image} onClick={() => characterSelction(props.id)}/>
           </div>
         </div>
       );
