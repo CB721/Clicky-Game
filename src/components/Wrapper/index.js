f59a51fb275db26c377eeb7d8ed98a3d3c752e98
Clint Brodar <clintbrodar@gmail.com>
2019-08-04 09:45:29 -0700

    Navbar

--- src/components/Wrapper/index.js
+++ src/components/Wrapper/index.js
@@ -2,7 +2,23 @@ import React from "react";
 import "./style.css";
 
 function Wrapper(props) {
-  return <div className="wrapper">{props.children}</div>;
+  return <div className="wrapper">
+    <nav className="navbar">
+      <ul>
+        <li className="brand">
+          <a href="/">Clicky Game</a>
+        </li>
+        <li>
+          Click an image to begin!
+        </li>
+        <li>
+          Score: 0 | Top Score: 0
+        </li>
+      </ul>
+    </nav>
+    {props.children}
+  </div>
+
 }
 
 export default Wrapper;
