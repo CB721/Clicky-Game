91b7de07a80f2c35d3ef7db75e102fdeb2900183
Clint Brodar <clintbrodar@gmail.com>
2019-08-07 21:47:54 -0700

    Updates

---
+++ precache-manifest.e7e541863f3ced4869cea38b62920d68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d76ee8621da8914781f0d700ec5d9998",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "9ff741d9bd60c04263f5",
+    "url": "/Clicky-Game/static/css/main.126a6765.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/Clicky-Game/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "9ff741d9bd60c04263f5",
+    "url": "/Clicky-Game/static/js/main.be1d1da0.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "62ad8347ddd9b1c2a2b2935cc328c788",
+    "url": "/Clicky-Game/static/media/mario.62ad8347.jpg"
+  }
+]);
